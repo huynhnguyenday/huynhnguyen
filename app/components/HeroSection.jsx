b8a4cac40991fc3f5a91ca30656ae739567d76fc
huynhnguyenday <nguyenhuuhuynh27022003@gmail.com>
2025-02-06 20:02:14 +0700

    button light night

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
-const HeroSection = () => {
+const HeroSection = ({ isDarkMode }) => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12 mt-10 lg:mt-28">
         {/* Phần chữ */}
         <div className="col-span-7 mx-2 lg:mt-8 lg:ml-20 lg:mr-0">
-          <h1 className="text-4xl lg:text-5xl mb-4 font-bold text-white">
+          <h1
+            className={`text-4xl lg:text-5xl mb-4 font-bold ${
+              isDarkMode ? "text-white" : "text-[#2A1454]"
+            }`}
+          >
             Hi, I am Huỳnh
           </h1>
-          <h2 className="text-4xl lg:text-6xl mb-10 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          <h2
+            className={`text-4xl lg:text-6xl mb-10 font-extrabold text-transparent bg-clip-text ${
+              isDarkMode
+                ? "bg-gradient-to-r from-purple-400 to-white"
+                : "bg-gradient-to-r from-purple-400 to-black"
+            }`}
+          >
             Front End Developer.
           </h2>
 
@@ -28,7 +38,11 @@ const HeroSection = () => {
               className="rounded-3xl rotate-6 hover:rotate-0 border-2 border-indigo-900 hover:border-purple-950 transition-all duration-300"
             />
           </div>
-          <p className="text-white text-2xl lg:text-2xl">
+          <p
+            className={`${
+              isDarkMode ? "text-white" : "text-[#2A1454]"
+            } text-2xl lg:text-2xl`}
+          >
             I like to focus on making my website simple but easy to use and
             optimized for users while still fully meeting the minimum uxui
             architecture of the website.
@@ -36,20 +50,34 @@ const HeroSection = () => {
 
           {/* Chỉnh sửa flex để các icon nằm ngang bên phải nút Download */}
           <div className="flex flex-col lg:flex-row items-center mt-12">
-            <button className="bg-black text-purple-400 font-semibold border-solid border border-purple-400 px-8 py-4 text-xl rounded-full flex items-center hover:bg-purple-400 hover:text-white transition-transform duration-200 hover:scale-95">
-              Download CV
-              <MdOutlineFileDownload className="ml-2 text-4xl font-thin" />
+            <button
+              className={`relative overflow-hidden ${
+                isDarkMode
+                  ? "bg-black text-purple-400"
+                  : "bg-white text-[#8750F7]"
+              } font-semibold border border-purple-400 px-8 py-4 text-xl rounded-full flex items-center transition-transform duration-300 hover:scale-95 group hover:text-white`}
+            >
+              <span className="relative z-10 flex items-center">
+                Download CV
+                <MdOutlineFileDownload className={`ml-2 text-4xl font-thin`} />
+              </span>
+              <span className="absolute inset-0 bg-purple-400 scale-y-0 origin-bottom transition-transform duration-500 group-hover:scale-y-100"></span>
             </button>
 
-            {/* Các icon nằm bên phải của nút Download trên laptop, nằm dưới nút trên mobile */}
             <div className="flex lg:ml-6 gap-4 mt-6 lg:mt-0">
               <a
                 href="https://www.facebook.com/imodnoliub"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <button className="w-12 h-12 flex items-center justify-center bg-black text-purple-400 border-solid border border-purple-400 rounded-full hover:bg-purple-400 hover:text-white transition-transform duration-200 hover:scale-95">
-                  <FaFacebookF className="text-xl" />
+                <button
+                  className={`w-12 h-12 flex items-center justify-center ${
+                    isDarkMode
+                      ? "bg-black text-purple-400"
+                      : "bg-white text-[#8750F7]"
+                  } border-solid border border-purple-400 rounded-full hover:bg-purple-400 hover:text-white transition-transform duration-200 hover:scale-95`}
+                >
+                  <FaFacebookF className="text-xl hover:text-white" />
                 </button>
               </a>
               <a
@@ -57,8 +85,14 @@ const HeroSection = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <button className="w-12 h-12 flex items-center justify-center bg-black text-purple-400 border-solid border border-purple-400 rounded-full hover:bg-purple-400 hover:text-white transition-transform duration-200 hover:scale-95">
-                  <FaLinkedinIn className="text-xl" />
+                <button
+                  className={`w-12 h-12 flex items-center justify-center ${
+                    isDarkMode
+                      ? "bg-black text-purple-400"
+                      : "bg-white text-[#8750F7]"
+                  } border-solid border border-purple-400 rounded-full hover:bg-purple-400 hover:text-white transition-transform duration-200 hover:scale-95`}
+                >
+                  <FaLinkedinIn className="text-xl hover:text-white" />
                 </button>
               </a>
               <a
@@ -66,8 +100,14 @@ const HeroSection = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <button className="w-12 h-12 flex items-center justify-center bg-black text-purple-400 border-solid border border-purple-400 rounded-full hover:bg-purple-400 hover:text-white transition-transform duration-200 hover:scale-95">
-                  <FaGithub className="text-xl" />
+                <button
+                  className={`w-12 h-12 flex items-center justify-center ${
+                    isDarkMode
+                      ? "bg-black text-purple-400"
+                      : "bg-white text-[#8750F7]"
+                  } border-solid border border-purple-400 rounded-full hover:bg-purple-400 hover:text-white transition-transform duration-200 hover:scale-95`}
+                >
+                  <FaGithub className="text-xl hover:text-white" />
                 </button>
               </a>
             </div>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,30 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import HeroSection from "./components/HeroSection";
+import { MdOutlineNightlight } from "react-icons/md";
+import { CiLight } from "react-icons/ci";
 
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(true); // State to track the mode (dark/light)
+
+  // Toggle between dark and light mode
+  const handleToggle = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  // Effect to apply the background color and text color based on the mode
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.style.setProperty("--background", "#0a0a0a");
+      document.documentElement.style.setProperty("--foreground", "#ededed");
+    } else {
+      document.documentElement.style.setProperty("--background", "#ffffff");
+      document.documentElement.style.setProperty("--foreground", "#171717");
+    }
+  }, [isDarkMode]);
+
+  // Starry sky effect function
   useEffect(() => {
     const generateStars = (numStars) => {
       const container = document.querySelector("main");
@@ -12,8 +33,8 @@ export default function Home() {
         const star = document.createElement("span");
         star.classList.add("star");
 
-        const randomX = Math.random(); 
-        const randomY = Math.random(); 
+        const randomX = Math.random();
+        const randomY = Math.random();
         const randomSize = Math.random() * 0.8 + 0.5;
         const randomDuration = Math.random() * 2 + 3;
 
@@ -35,8 +56,28 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="relative flex min-h-screen flex-col bg-[#121212] mx-auto px-12 py-6">
-      <HeroSection />
+    <main className="relative flex min-h-screen flex-col bg-[var(--background)] text-[var(--foreground)] mx-auto px-12 py-6 transition-all duration-300">
+      <HeroSection isDarkMode={isDarkMode} />
+
+      {/* Dark/Light Mode Toggle Button */}
+      <button
+        onClick={handleToggle}
+        className="absolute top-40 lg:top-80 h-10 w-14 text-white group hover:w-32 right-0 flex items-center justify-center p-2 bg-gradient-to-r from-purple-400 hover:from-violet-800 hover:to-purple-400 to-violet-800 text-2xl border-none shadow-none rounded-l-full transition-all duration-300"
+      >
+        <div className="flex items-left">
+          {isDarkMode ? (
+            <>
+              <MdOutlineNightlight size={30} />
+              <span className="ml-2 hidden group-hover:inline">Light</span>
+            </>
+          ) : (
+            <>
+              <CiLight size={30} />
+              <span className="ml-2 hidden group-hover:inline">Night</span>
+            </>
+          )}
+        </div>
+      </button>
     </main>
   );
 }
